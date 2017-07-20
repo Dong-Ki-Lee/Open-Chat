3045140dc009209dd39e5aff4bd8b989e5fccd6b
Dong-Ki-Lee <dlehdrl9638@gmail.com>
2017-07-20 21:22:38 +0900

    apply SearchController

--- openCommunity/src/work/controller/SearchController.java
+++ openCommunity/src/work/controller/SearchController.java
@@ -3,6 +3,7 @@ package work.controller;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,34 +14,79 @@ import work.model.dao.BoardSearchDao;
 import work.model.dto.MembersInfo;
 import work.model.dto.NoticeBoards;
 
-/**
- * Servlet implementation class SearchController
- */
+
 public class SearchController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public SearchController() {
         super();
         // TODO Auto-generated constructor stub
     }
-
     protected void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String action = request.getParameter("action");
-		System.out.println("\n## action : " + action);
-		
-		switch(action) {
-			case "searchBoard":
-				searchBoard(request, response);
-				break;
-			default:	
-		}	
-		
+	    String action = request.getParameter("action");
+	    System.out.println("\n## action : " + action);
+	
+	    switch(action) {
+	    case "searchBoard":
+	        searchBoard(request, response);
+	        break;
+	    case "recommandExtend":
+	        recommandExtend(request, response);
+	        break;
+	    case "subscribeExtend":
+	        subscribeExtend(request, response);
+	        break;
+	    default:
+	    }
+	
 	}
-    
-    private BoardSearchDao boardSearchDao = new BoardSearchDao();
+	
+	private BoardSearchDao boardSearchDao = new BoardSearchDao();
+	
+	protected void subscribeExtend(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    HttpSession session = request.getSession(false);
+	
+	    if (session != null && session.getAttribute("memberNo") != null) {
+	        int memberNo = (int)session.getAttribute("memberNo");
+	
+	        ArrayList<NoticeBoards> subscribeBoard = boardSearchDao.getSubscribeBoard(memberNo);
+	
+	        request.setAttribute("boardList", subscribeBoard);
+	        RequestDispatcher dispatcher = request.getRequestDispatcher("/extendMemberPage.jsp");
+	        dispatcher.forward(request, response);
+	    } else {
+	        request.setAttribute("Message", "로그인 후 사용하시기 바랍니다.");
+	        request.getRequestDispatcher("/error.jsp").forward(request, response);
+	    }
+	}
+	
+	
+	protected void recommandExtend(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    HttpSession session = request.getSession(false);
+	
+	    if (session != null && session.getAttribute("memberNo") != null) {
+	        int memberNo = (int)session.getAttribute("memberNo");
+	
+	        ArrayList<String> searchHistory = boardSearchDao.getSearchHistory(memberNo);
+	
+	        ArrayList<NoticeBoards> matchBoard = null;
+	        if(searchHistory == null || searchHistory.size() == 0) {
+	            matchBoard = boardSearchDao.getLargeBoard();
+	        } else {
+	            matchBoard = boardSearchDao.searchBoardWithTag(searchHistory);
+	        }
+	
+	        /*ArrayList<NoticeBoards> subscribeBoard = boardSearchDao.getSubscribeBoard(memberNo);
+	
+	         request.setAttribute("subscribeBoard", subscribeBoard);*/
+	        request.setAttribute("boardList", matchBoard);
+	        RequestDispatcher dispatcher = request.getRequestDispatcher("/extendMemberPage.jsp");
+	        dispatcher.forward(request, response);
+	    } else {
+	        request.setAttribute("Message", "로그인 후 사용하시기 바랍니다.");
+	        request.getRequestDispatcher("/error.jsp").forward(request, response);
+	    }
+	}
 	protected void searchBoard(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("\n## action : searchBoard()");
 		// 2. 요청 데이터 추출 : join.jsp
